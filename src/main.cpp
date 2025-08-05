7ec1a65c35cc820aeb728582d9817517d6043982
iwnmw <isaacbwoodward@gmail.com>
2025-08-05 14:14:37 -0500

    have encoder feedback working, but need PID to get accurate control of motor

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <Encoder.h>
 
 // Define pin variables
 const int INA = 2;
@@ -9,6 +10,15 @@ const int PWM_PIN = 3;
 const int maxSpeed = 530; // Maximum speed in RPM
 float commandedSpeed = 530.0; // Commanded speed in RPM
 int commandedDirection = -1; // 1 for forward, -1 for reverse, 0 for stop
+
+// Define variables for encoder reading and initialize object
+const int encoderPinA = 5; // Must be interrupt capable; keep in mind if you change to another board
+const int encoderPinB = 6;
+Encoder myEnc(encoderPinA, encoderPinB);
+
+// Define values for converting the encoder readings to speed (RPM)
+const int encoderCPR = 1200; // Counts per revolution of the encoder (output shaft)
+unsigned long lastTime = 0;
 
 // Define a function to convert from rotational speed to PWM output
 int speedToPWM(int commandedSpeed) {
@@ -34,14 +44,33 @@ void sendMotorOutput(int speed, int direction) {
 
 void setup() {
   Serial.begin(9600);
+
+  // Initialize pins for motor control
   pinMode(INA, OUTPUT);
   pinMode(INB, OUTPUT);
   pinMode(PWM_PIN, OUTPUT);
   Serial.println("Motor Control Initialized");
 }
 
+long oldPosition  = 0;
+
 void loop() {
   // Output commanded speed to the motor
   int pwmValue = speedToPWM(commandedSpeed);
   sendMotorOutput(pwmValue, commandedDirection);
+  Serial.print("Commanded Speed (PWM): ");
+  Serial.println(pwmValue);
+
+  // Read the encoder position and calculate speed
+  long newPosition = myEnc.read();
+  unsigned long currentTime = millis();
+  if (currentTime - lastTime >= 100) { // Update every 100 ms
+    long deltaPosition = newPosition - oldPosition;
+    float speedRPM = (deltaPosition / (float)encoderCPR) * (60000.0 / (currentTime - lastTime));
+    Serial.print("Measured Speed (RPM): ");
+    Serial.println(speedRPM);
+    lastTime = currentTime;
+    oldPosition = newPosition;
+    delay(500);
+  }
 }
